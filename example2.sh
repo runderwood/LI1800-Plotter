0bbed98e355cf98dc0c27a3a42cd66959f03a601
Reed <reed@fm2279.com>
2011-12-13 15:02:37 -0500

    example2

--- example2.sh
+++ example2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 python li1800plot.py \
-    --outfile "li1800.topandbottom.pdf" \
+    --outfile "li1800.w_asd.pdf" \
     --title "Spathiphyllum Leaf Spectra" \
     --yaxis "" \
-    --infiles reft.txt refb.txt ../asd2/06topref.mn.txt \
+    --infiles data/reft.txt data/refb.txt data/asd.topref.mn.txt data/asd.botref.mn.txt \
     --labels "Top Reflectance" "Top Transmittance" "Top Absorptance" \
         "Bottom Reflectance" "Bottom Transmittance" "Bottom Absorptance"
 
